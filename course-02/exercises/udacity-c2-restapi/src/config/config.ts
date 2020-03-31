bce5ad0ef834da7ae1f94859f1540a1b1ccadc2d
cklasicki <cklasicki@gmail.com>
2020-03-31 23:11:54 +0200

    added dev db configuration

--- course-02/exercises/udacity-c2-restapi/src/config/config.ts
+++ course-02/exercises/udacity-c2-restapi/src/config/config.ts
@@ -1,13 +1,13 @@
 export const config = {
   "dev": {
-    "username": "",
-    "password": "",
-    "database": "",
-    "host": "",
+    "username": "udagramczarekdev",
+    "password": "udagramczarekdev",
+    "database": "udagramczarekdev",
+    "host": "udagramczarekdev.co1x4xeosg2t.us-east-1.rds.amazonaws.com",
     "dialect": "postgres",
-    "aws_region": "us-east-2",
+    "aws_region": "us-east-1",
     "aws_profile": "default",
-    "aws_media_bucket": "udagram-ruttner-dev"
+    "aws_media_bucket": "udagrammediadev"
   },
   "prod": {
     "username": "",
